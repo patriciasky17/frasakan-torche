974c145b12d26ac7991882af66c91ff44b7edc86
Patricia Ho <hopatricia1711@gmail.com>
2023-01-11 23:47:40 +0700

    erase unnecessary function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,3 @@
-// const menuToggle = document.querySelector('.menu-toggle input');
-// const nav = document.querySelector('nav ul');
-
-// menuToggle.addEventListener('click', function() {
-//     nav.classList.toggle('slide');
-// });
-
-// const navbar = document.getElementById('nav');
-// window.onscroll = () => {
-//     if (window.scrollY >= 100) {
-//         navbar.classList.add('nav-active');
-//     } else {
-//         navbar.classList.remove('nav-active');
-//     }
-// };
-
-/**
-* Template Name: NewBiz - v4.7.0
-* Template URL: https://bootstrapmade.com/newbiz-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
